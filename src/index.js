8df6774d18a5e81304335a9bddd63c557a19ae02
abdelbaki <bouamamaabdelbaki@gmail.com>
2023-06-25 15:03:33 +0200

    Change the / route  message

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ const express = require('express');
 
 const app = express();
 
-app.get('/',(req,res)=>{
-    res.status(200).send('get to the application')
+app.get('/', (req, res) => {
+    res.status(200).send('App is running on port 4000')
 })
 
-app.listen(4000,()=>{
+app.listen(4000, () => {
     console.log('app is runing on port 4000');
 })
