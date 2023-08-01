6f88e57cc5f52d124e4ac95d36a92f650cb98e8e
Bouamama Abdelbaki <bouamamaabdelbaki@gmail.com>
2023-08-01 15:30:15 +0100

    Update login.controller.js

--- src/controllers/login.controller.js
+++ src/controllers/login.controller.js
@@ -1,5 +1,6 @@
 const userModel = require("../models/user.model");
-
+const dotenv =  require('dotenv');
+dotenv.config()
 const login = async (req, res) => {
     const enVariable_B = process.env.TEST_BAKI || "default value";
   const enVariable_T = process.env.TEST_TAKI || "default value";
