27fa0b07feb5122923b0e496ea7ef68e0740e597
abdelbaki bouamama <abdelbaki.bouamama@yassir.com>
2023-08-01 09:52:12 +0100

    add environment variablese

--- src/controllers/login.controller.js
+++ src/controllers/login.controller.js
@@ -1,6 +1,10 @@
 const userModel = require("../models/user.model");
+const dotenv = require("dotenv");
+dotenv.config();
 
 const login = async (req, res) => {
+  const enVariable = process.env.TEST_BAKI;
+  console.log("tatatat", enVariable);
   const user = await userModel.create({
     name: "abdelbaki",
     address: "sidi amar",
