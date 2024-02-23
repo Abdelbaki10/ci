67219148cee62adbd08570b73146f002868ea018
Bouamama Abdelbaki <bouamamaabdelbaki@gmail.com>
2024-02-23 23:39:19 +0100

    tes remote repo extension

--- src/tests/firstTest.test.js
+++ src/tests/firstTest.test.js
@@ -1,6 +1,6 @@
-describe('testing github ci/cd pipline',()=>{
-    test('expect 1 plus 1 to be 2',()=>{
-        expect(1+1).toBe(2);
-        expect(1+1).not.toBe(3);
-    })
-})
+describe("testing github ci/cd pipline", () => {
+  test("expect 1 plus 1 to be 2", () => {
+    expect(1 + 1).toBe(2);
+    expect(1 + 1).not.toBe(3);
+  });
+});
